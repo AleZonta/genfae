41ae2e59dad7f9ed632b0359803ff7225e7919e2
Alessandro <salvarosacity@gmail.com>
2020-04-30 12:19:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Generative Fourier-based Auto-Encoders
+
+Code used to generate the results presented in Generative Fourier-based Auto-Encoders: Preliminary Results paper
+
--- src/data_loader/loader.py
+++ src/data_loader/loader.py
@@ -1,5 +1,5 @@
 """
-Generative Fourier-based Auto-Encoders:Preliminary Results
+git init
 Copyright (C) 2018  Alessandro Zonta (a.zonta@vu.nl)
 
 This program is free software: you can redistribute it and/or modify
